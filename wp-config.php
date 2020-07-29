071956d61c480c451bb8dbd67ac56e22c080f7b5
dl <dl@Dmitriys-MacBook-Pro.local>
2020-07-30 01:55:15 +0300

    changed site url to https

--- wp-config.php
+++ wp-config.php
@@ -88,3 +88,6 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 /** Sets up WordPress vars and included files. */
 require_once( ABSPATH . 'wp-settings.php' );
+
+define( 'WP_HOME', 'https://bitcoincasinos.top' );
+define( 'WP_SITEURL', 'https://bitcoincasinos.top' );
